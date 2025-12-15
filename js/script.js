cb99a56c7c60d3b997c2806a86e4209601ec6c33
Aaroncillo016 <aaronmorenofernandez63@gmail.com>
2025-12-15 13:21:05 +0100

    Primera Subida

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function cambiarColor() {
+            const colores = [
+                "linear-gradient(135deg, #ff9a9e, #fad0c4)",
+                "linear-gradient(135deg, #a1c4fd, #c2e9fb)",
+                "linear-gradient(135deg, #fddb92, #d1fdff)",
+                "linear-gradient(135deg, #9890e3, #b1f4cf)",
+                "linear-gradient(135deg, #fbc2eb, #a6c1ee)"
+            ];
+            const random = Math.floor(Math.random() * colores.length);
+            document.body.style.background = colores[random];
+        }
