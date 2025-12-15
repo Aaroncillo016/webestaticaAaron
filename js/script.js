3851151962ea3adadc3d6ba504932f685a6c9a5b
Aaroncillo016 <aaronmorenofernandez63@gmail.com>
2025-12-15 13:25:18 +0100

    cambios de prueba

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function cambiarColor() {
-            const colores = [
+            const colorin = [
                 "linear-gradient(135deg, #ff9a9e, #fad0c4)",
                 "linear-gradient(135deg, #a1c4fd, #c2e9fb)",
                 "linear-gradient(135deg, #fddb92, #d1fdff)",
                 "linear-gradient(135deg, #9890e3, #b1f4cf)",
                 "linear-gradient(135deg, #fbc2eb, #a6c1ee)"
             ];
-            const random = Math.floor(Math.random() * colores.length);
-            document.body.style.background = colores[random];
+            const random = Math.floor(Math.random() * colorin.length);
+            document.body.style.background = colorin[random];
         }
